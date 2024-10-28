de339aa5b4eb8116b6cb7aeb103d65e8f6275018
ZG_DEV <159061718+ZG089@users.noreply.github.com>
2024-10-28 21:26:47 +0300

    initial commit

---
+++ omni_CPH1909.mk
@@ -0,0 +1,16 @@
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := CPH1909
+PRODUCT_NAME := omni_$(PRODUCT_DEVICE)
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := CPH1909
+PRODUCT_MANUFACTURER := OPPO
+
+# Default device path for tree
+DEVICE_PATH := device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)
